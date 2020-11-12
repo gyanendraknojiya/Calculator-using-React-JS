1beacc8dac24e05d01df8385cec4e5f7e58183de
gyanendra kumar <gyanendrak064@gmail.com>
2020-11-12 20:58:48 +0530

    clear button bug fixed

---
+++ build/precache-manifest.8a3ba9b883de365adbbc1dfafa59b1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f77cedb1f79bd5a27d96bb5a93f8e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b85c76cacdda139def9b",
+    "url": "/static/js/2.2f5a74f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5a74f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25f619e5d684e1ac709",
+    "url": "/static/js/main.ac641338.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
