17a24873392fc47e4f5490944ab7ffa0a9ae839b
gyanendra kumar <gyanendrak064@gmail.com>
2020-11-12 21:03:50 +0530

    clear button bug fixed

---
+++ docs/precache-manifest.6b17e98bfd32e3e5b8b1249b10d7a3f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a499184d9e75270590220f6c85d39f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b85c76cacdda139def9b",
+    "url": "./static/js/2.2f5a74f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2f5a74f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25f619e5d684e1ac709",
+    "url": "./static/js/main.ac641338.chunk.js"
+  },
+  {
+    "revision": "7a833fefead15dc83aa8",
+    "url": "./static/js/runtime-main.d6a7b5d6.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a3ba9b883de365adbbc1dfafa59b1dd.js"
+  "./precache-manifest.6b17e98bfd32e3e5b8b1249b10d7a3f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
